d30d42ceac68983fc9410d9540e8df080fd96bd9
Chengguang Li <Chengguang.Li@vertiv.com>
2023-10-08 17:24:34 +0800

    chore(project):初始化项目

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// 样式引用顺序：
+// 1. 初始化样式要放到最前
+import 'reset-css';
+// 2. UI框架的样式
+
+// 3. 全局样式
+import './assets/styles/global.scss'
+
+// 4. 组件的样式
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
