d1e970e81838a16959fa76d03c820fc709990115
thesecretofwind <2586404691@qq.com>
2023-10-10 22:34:32 +0800

    chore(project):配置antd按需加载

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// antd5.x版本以上，不需要使用vite-plugin-style-import来进行样式的按需引入，只用import {Button}既可使用。
+// import styleImport, {AntdResolve} from 'vite-plugin-style-import'
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+  // styleImport({
+  //   resolves:[AntdResolve()]
+  // })
+],
   resolve: {
     alias:{
       '@':path.resolve(__dirname, './src')
