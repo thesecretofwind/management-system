4d3166b5bdf2727fb1f53c6c286a183fc310f037
thesecretofwind <2586404691@qq.com>
2023-10-12 22:26:00 +0800

    feat(component):更新首页组件

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@
 // import Comp2 from "./components/Comp2"
 // import { Button } from "antd"
 // import {RightCircleFilled} from '@ant-design/icons'
-import {  Link, useRoutes } from "react-router-dom"
+import { useRoutes } from "react-router-dom"
 import routes from "./router"
 import { Suspense } from "react";
 function App() {
@@ -17,8 +17,8 @@ function App() {
      <Comp2></Comp2>
      <Button type="primary">click</Button>
      <RightCircleFilled style={{fontSize: '20px', color: 'red'}}/> */}
-     <Link to="/home">home</Link>
-     <Link  to="/about">about</Link>
+     {/* <Link to="/home">home</Link>
+     <Link  to="/about">about</Link> */}
      {/* 路由组件渲染位置 (这里是原始用来渲染路由的方式)*/}
      {/* <Outlet></Outlet> */}
      <Suspense fallback={<div>loading...</div>}>
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,7 +1,80 @@
-const Home = () => (
-  <div className="home">
-    home组件
-  </div>
-)
+import React, { useState } from 'react';
+import {
+  DesktopOutlined,
+  FileOutlined,
+  PieChartOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+
+const { Header, Content, Footer, Sider } = Layout;
+
+type MenuItem = Required<MenuProps>['items'][number];
+
+function getItem(
+  label: React.ReactNode,
+  key: React.Key,
+  icon?: React.ReactNode,
+  children?: MenuItem[],
+): MenuItem {
+  return {
+    key,
+    icon,
+    children,
+    label,
+  } as MenuItem;
+}
+
+const items: MenuItem[] = [
+  getItem('Option 1', '1', <PieChartOutlined />),
+  getItem('Option 2', '2', <DesktopOutlined />),
+  getItem('User', 'sub1', <UserOutlined />, [
+    getItem('Tom', '3'),
+    getItem('Bill', '4'),
+    getItem('Alex', '5'),
+  ]),
+  getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
+  getItem('Files', '9', <FileOutlined />),
+];
+
+const Home: React.FC = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  const menuClick =(e:MenuItem) => {
+    console.log(e);
+    
+  }
+
+  return (
+    <Layout style={{ minHeight: '100vh' }}>
+      {/* 左边侧边栏 */}
+      <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+        <div className="demo-logo-vertical" />
+        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items}  onClick={menuClick}/>
+      </Sider>
+      {/* 右侧内容区 */}
+      <Layout>
+        <Header style={{ paddingLeft:'16px', background: colorBgContainer }} >
+          <Breadcrumb style={{ lineHeight: '64px' }} items={[{title: 'User'}, {title: 'Bill'}]}>
+            {/* <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Bill</Breadcrumb.Item> */}
+          </Breadcrumb>
+        </Header>
+        <Content style={{ margin: '16px 16px 0', height: '100%' }} className="demo-logo-vertical">
+          <div style={{ padding: 24, height: '100%', background: colorBgContainer }}>
+            Bill is a cat.
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center', padding: 0, lineHeight:"48px" }}>Ant Design ©2023 Created by Ant UED</Footer>
+      </Layout>
+    </Layout>
+  );
+};
+
 
 export default Home
