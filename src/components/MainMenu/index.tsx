b1168f32884c15c67e70ad37c64a2554eaf38430
thesecretofwind <2586404691@qq.com>
2023-10-14 21:55:29 +0800

    fix(component)：侧边栏menu转换诶嵌套树的形式

--- src/components/MainMenu/index.tsx
+++ src/components/MainMenu/index.tsx
@@ -13,34 +13,91 @@ import { useNavigate } from "react-router-dom";
 
 type MenuItem = Required<MenuProps>["items"][number];
 
-function getItem(
-  label: React.ReactNode,
-  key: React.Key,
-  icon?: React.ReactNode,
-  children?: MenuItem[]
-): MenuItem {
-  return {
-    key,
-    icon,
-    children,
-    label,
-  } as MenuItem;
-}
+// function getItem(
+//   label: React.ReactNode,
+//   key: React.Key,
+//   icon?: React.ReactNode,
+//   children?: MenuItem[]
+// ): MenuItem {
+//   return {
+//     key,
+//     icon,
+//     children,
+//     label,
+//   } as MenuItem;
+// }
 
-const items: MenuItem[] = [
-  getItem("Option 1", "/page1", <PieChartOutlined />),
-  getItem("Option 2", "/page2", <DesktopOutlined />),
-  getItem("User", "sub1", <UserOutlined />, [
-    getItem("Tom", "3"),
-    getItem("Bill", "4"),
-    getItem("Alex", "5"),
-  ]),
-  getItem("Team", "sub2", <TeamOutlined />, [
-    getItem("Team 1", "6"),
-    getItem("Team 2", "8"),
-  ]),
-  getItem("Files", "9", <FileOutlined />),
-];
+// const items: MenuItem[] = [
+//   getItem("Option 1", "/page1", <PieChartOutlined />),
+//   getItem("Option 2", "/page2", <DesktopOutlined />),
+//   getItem("User", "sub1", <UserOutlined />, [
+//     getItem("Tom", "3"),
+//     getItem("Bill", "4"),
+//     getItem("Alex", "5"),
+//   ]),
+//   getItem("Team", "sub2", <TeamOutlined />, [
+//     getItem("Team 1", "6"),
+//     getItem("Team 2", "8"),
+//   ]),
+//   getItem("Files", "9", <FileOutlined />),
+// ];
+
+// 转换为嵌套树的形式，更直观，以方便后端返回数据后渲染侧边栏menu
+const items: MenuItem[] = [ 
+  {
+    label:'栏目1',
+    key: '/page1',
+    icon:<PieChartOutlined />,
+  },
+  {
+    label:'栏目2',
+    key: '/page2',
+    icon:<DesktopOutlined />
+  },
+  {
+    label:'栏目3',
+    key: '/page3',
+    icon: <UserOutlined />,
+    children: [
+      {
+        label:'栏目301',
+        key: '/page301',
+      },
+      {
+        label:'栏目302',
+        key: '/page302',
+      },      
+      {
+        label:'栏目303',
+        key: '/page3011',
+      },
+      {
+        label:'栏目304',
+        key: '/page304',
+      },
+    ]
+  },
+  {
+    label:'栏目4',
+    key: '/page4',
+    icon: <TeamOutlined />,
+    children: [
+      {
+        label:'栏目401',
+        key: '/page401',
+      },
+      {
+        label:'栏目402',
+        key: '/page402',
+      }
+    ]
+  },
+  {
+    label:'栏目5',
+    key: '/page5',
+    icon: <FileOutlined />
+  },
+]
 
 // const rootSubmenuKeys = ['sub1', 'sub2'];
 
