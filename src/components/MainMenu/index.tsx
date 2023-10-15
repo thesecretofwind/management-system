40c1cbfb4125511f2bc0cec468217ea791bc94aa
thesecretofwind <2586404691@qq.com>
2023-10-15 15:08:40 +0800

    fix(component)：修改侧边栏menu只能展开一个，点击一级菜单会收起

--- src/components/MainMenu/index.tsx
+++ src/components/MainMenu/index.tsx
@@ -8,7 +8,7 @@ import {
 } from "@ant-design/icons";
 import type { MenuProps } from "antd";
 import { Menu } from "antd";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 
 type MenuItem = Required<MenuProps>["items"][number];
@@ -43,7 +43,7 @@ type MenuItem = Required<MenuProps>["items"][number];
 // ];
 
 // 转换为嵌套树的形式，更直观，以方便后端返回数据后渲染侧边栏menu
-const items: MenuItem[] = [ 
+const items = [ 
   {
     label:'栏目1',
     key: '/page1',
@@ -56,39 +56,39 @@ const items: MenuItem[] = [
   },
   {
     label:'栏目3',
-    key: '/page3',
+    key: 'page3', // 注意：有子menu的key值好像不用加上斜杠/
     icon: <UserOutlined />,
     children: [
       {
         label:'栏目301',
-        key: '/page301',
+        key: '/page3/page301',
       },
       {
         label:'栏目302',
-        key: '/page302',
+        key: '/page3/page302',
       },      
       {
         label:'栏目303',
-        key: '/page3011',
+        key: '/page3/page3011',
       },
       {
         label:'栏目304',
-        key: '/page304',
+        key: '/page3/page304',
       },
     ]
   },
   {
     label:'栏目4',
-    key: '/page4',
+    key: 'page4',
     icon: <TeamOutlined />,
     children: [
       {
         label:'栏目401',
-        key: '/page401',
+        key: '/page4/page401',
       },
       {
         label:'栏目402',
-        key: '/page402',
+        key: '/page4/page402',
       }
     ]
   },
@@ -103,14 +103,37 @@ const items: MenuItem[] = [
 
 const MainMenu: React.FC = () => {
   const navigateTo = useNavigate();
-  const [openKeys, setOpenKeys] = useState([] as string[]);
+  const currentRoute = useLocation();
 
   const menuClick = (e: MenuItem) => {
     const url = e!.key as string;
     navigateTo(url);
+    // 点击没有二级菜单的menu直接将展开的二级菜单直接收齐
+    const hasChildren = items.some(item => item.key === url);
+    console.log(hasChildren);
+    if (hasChildren && openKeys.length !== 0) {
+      setOpenKeys([])
+    }
   };
+  // 如果是二级菜单路由，就要展开，此时就要将currentRoute.pathname与所有二级菜单的key值作比较，如果存在则设置父级的key为openKeys
+  let firstOpenKeys = '';
+  for (const item of items) {
+    if (!item.children) {
+      continue;
+    }
+    const children =  item.children;
+    const targetChild = children.find(child => child.key === currentRoute.pathname);
+    if (targetChild) {
+      firstOpenKeys = item.key;
+      break;
+    }
+  }
+
+  const [openKeys, setOpenKeys] = useState([firstOpenKeys]);
 
   const handleOpenChange = (openKeys: string[]) => {
+    console.log(openKeys);
+    
     // 所有点开的二级菜单是否保存
     // const latestOpenkey = keys.find((key: string) => !openKeys.includes(key));
     // // 如果点开的二级菜单中没保存，且所有二级菜单中不存在中，则直接设置
@@ -127,7 +150,8 @@ const MainMenu: React.FC = () => {
   return (
         <Menu
           theme="dark"
-          defaultSelectedKeys={["/page1"]}
+          // 当前选中项，对应Item的key值
+          defaultSelectedKeys={[currentRoute.pathname]}
           mode="inline"
           items={items}
           onClick={menuClick}
