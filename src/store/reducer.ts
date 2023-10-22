b394440fe773e9aefb48012be1bc6d8343a7594a
thesecretofwind <2586404691@qq.com>
2023-10-22 11:00:34 +0800

    feat(project):添加Action类型，以及通过useDispatch修改redux内容

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -3,10 +3,21 @@ export interface STATE {
   num: number;
 }
 
+export interface ACTION {
+  type: string;
+  value: STATE[keyof STATE];
+}
+
 const INIT_STATE: STATE = {
   num: 20
 }
-const reducer = (state = INIT_STATE) => {
+const reducer = (state = INIT_STATE, action: ACTION):STATE => {
+  switch(action.type) {
+    case 'selfAdd':
+      return {...state, num: state.num + 1};
+    case 'add':
+      return {...state, num: state.num + action.value};
+  }
   return {...state};
 };
 
--- src/views/Page1.tsx
+++ src/views/Page1.tsx
@@ -1,10 +1,20 @@
 import { STATE } from "@/store/reducer";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+
 
 const Page1 = () => { 
-  const { num } = useSelector((state:STATE) => ({num:state.num}))
+  // 通过 useSelector 获取redux中的数据
+  const { num } = useSelector((state:STATE) => ({num:state.num}));
+  // 通过 useDispatch修改redux中的数据
+  const dispatch = useDispatch();
+  const changeNum = () =>{
+    dispatch({type: 'selfAdd', value: 2});
+  }
   return (
-  <div>这里是page1页面  { num }</div>
+    <>
+    <div>这里是page1页面  { num }</div>
+    <button onClick={changeNum}>按钮鸭</button>
+  </>
 )}
 
 export default Page1;
